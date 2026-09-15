15dd574548d91a6c098585e8153190174083c457
viktorikik <z4dp89bzjn@privaterelay.appleid.com>
2026-09-15 18:50:40 +0700

    Create Constants.js
    Создание файла

---
+++ js/utils/Constants.js
@@ -0,0 +1,151 @@
+// Статусы
+export const STATUSES = {
+  DONE: 'done',
+  PLANNED: 'planned'
+};
+
+// Категории
+export const CATEGORIES = {
+  SOUP: 'soup',
+  SALAD: 'salad',
+  MAIN: 'main',
+  OTHER: 'other'
+};
+
+export const CATEGORY_LABELS = {
+  [CATEGORIES.SOUP]: '🍲 Супы',
+  [CATEGORIES.SALAD]: '🥗 Салаты',
+  [CATEGORIES.MAIN]: '🍖 Основные блюда',
+  [CATEGORIES.OTHER]: '🍽️ Другое'
+};
+
+// Список продуктов для распознавания ингредиентов
+export const PRODUCT_WORDS = [
+  'лук', 'морковь', 'картофель', 'капуста', 'свекла', 'редис', 'репа',
+  'огурец', 'помидор', 'перец', 'баклажан', 'кабачок', 'тыква',
+  'чеснок', 'зелень', 'петрушка', 'укроп', 'базилик', 'кинза',
+  'салат', 'шпинат', 'щавель', 'ревень', 'сельдерей',
+  'рис', 'гречка', 'овсянка', 'перловка', 'пшено', 'кускус', 'булгур',
+  'макароны', 'паста', 'лапша', 'вермишель', 'спагетти',
+  'говядина', 'свинина', 'баранина', 'телятина', 'курица', 'индейка',
+  'утка', 'гусь', 'кролик', 'фарш', 'печень', 'сердце', 'почки',
+  'тунец', 'горбуша', 'лосось', 'форель', 'сёмга', 'кета', 'кижуч',
+  'треска', 'пикша', 'окунь', 'судак', 'щука', 'сом', 'налим',
+  'осётр', 'пангасиус', 'тилапия', 'дорада', 'сибас', 'ставрида',
+  'скумбрия', 'макрель', 'сельдь', 'шпроты', 'килька',
+  'креветки', 'мидии', 'кальмары', 'краб',
+  'молоко', 'сливки', 'сметана', 'йогурт', 'кефир', 'ряженка',
+  'творог', 'сыр', 'масло', 'маргарин', 'майонез', 'кетчуп',
+  'яйцо',
+  'соль', 'перец', 'сахар', 'мука', 'крахмал',
+  'корица', 'ваниль', 'какао', 'шоколад',
+  'орегано', 'тимьян', 'розмарин', 'кориандр', 'тмин', 'кумин',
+  'паприка', 'куркума', 'имбирь', 'шафран', 'гвоздика', 'кардамон',
+  'яблоко', 'груша', 'айва', 'хурма', 'гранат',
+  'лимон', 'лайм', 'грейпфрут', 'мандарин', 'апельсин',
+  'клубника', 'малина', 'черника', 'ежевика', 'смородина', 'крыжовник',
+  'вишня', 'черешня', 'слива', 'абрикос', 'персик', 'нектарин',
+  'банан', 'киви', 'манго', 'ананас', 'гранат',
+  'арбуз', 'дыня',
+  'финик', 'инжир', 'курага', 'чернослив', 'изюм',
+  'орех', 'миндаль', 'фисташка', 'кешью', 'грецкий орех', 'фундук',
+  'гриб', 'шампиньон', 'белый гриб', 'подберёзовик', 'лисичка',
+  'вода', 'бульон', 'вино', 'коньяк', 'ром', 'пиво',
+  'чай', 'кофе', 'какао', 'компот', 'кисель', 'морс', 'квас',
+  'лимонад', 'сок', 'нектар',
+  'оливковое масло', 'подсолнечное масло', 'сливочное масло',
+  'варенье', 'джем', 'конфитюр', 'мёд', 'сироп', 'пастила', 'мармелад',
+  'дрожжи', 'разрыхлитель', 'сода', 'уксус', 'лимонная кислота'
+];
+
+// Объект с ключами хранилища, событиями и селекторами
+export const CONSTANTS = {
+  STORAGE_KEYS: {
+    RECIPES: 'smartMenuRecipes_v1',
+    DISHES: 'smartMenuDishes_v5',
+    THEME: 'mealPlannerTheme'
+  },
+  EVENTS: {
+    DISHES_CHANGED: 'dishes:changed',
+    RECIPES_CHANGED: 'recipes:changed'
+  },
+  SELECTORS: {
+    monthTitle: 'monthTitle',
+    calendarContent: 'calendarContent',
+    menuContent: 'menuContent',
+    menuPeriod: 'menuPeriod',
+    modalOverlay: 'modalOverlay',
+    modalTitle: 'modalTitle',
+    modalContent: 'modalContent',
+    recOverlay: 'recOverlay',
+    recTitle: 'recTitle',
+    recContent: 'recContent',
+    themeToggle: 'themeToggle',
+    searchInput: 'searchInput',
+    statusFilter: 'statusFilter',
+    categoryFilter: 'categoryFilter',
+    viewToggle: 'viewToggle',
+    modalClose: 'modalClose',
+    recClose: 'recClose',
+    addModalOverlay: 'addModalOverlay',
+    addModalClose: 'addModalClose',
+    addModalCancel: 'addModalCancel',
+    newDishName: 'newDishName',
+    newDishNote: 'newDishNote',
+    newDishDate: 'newDishDate',
+    newDishStatus: 'newDishStatus',
+    newDishCategory: 'newDishCategory',
+    addModalSave: 'addModalSave',
+    exportModalOverlay: 'exportModalOverlay',
+    exportModalClose: 'exportModalClose',
+    choiceOverlay: 'choiceOverlay',
+    choiceClose: 'choiceClose',
+    choiceFromMenu: 'choiceFromMenu',
+    choiceFromTaste: 'choiceFromTaste',
+    choiceFromRecipes: 'choiceFromRecipes',
+    welcomeOverlay: 'welcomeOverlay',
+    welcomeStartBtn: 'welcomeStartBtn',
+    recipesOverlay: 'recipesOverlay',
+    recipesClose: 'recipesClose',
+    recipesList: 'recipesList',
+    addRecipeBtn: 'addRecipeBtn',
+    recipeFormOverlay: 'recipeFormOverlay',
+    recipeFormClose: 'recipeFormClose',
+    recipeFormTitle: 'recipeFormTitle',
+    recipeFormId: 'recipeFormId',
+    recipeName: 'recipeName',
+    recipeIngredients: 'recipeIngredients',
+    recipeInstructions: 'recipeInstructions',
+    recipeCategory: 'recipeCategory',
+    recipeFormCancel: 'recipeFormCancel',
+    recipeFormSave: 'recipeFormSave',
+    recipeParseBtn: 'recipeParseBtn',
+    shoppingListOverlay: 'shoppingListOverlay',
+    shoppingListClose: 'shoppingListClose',
+    shoppingDateFrom: 'shoppingDateFrom',
+    shoppingDateTo: 'shoppingDateTo',
+    generateShoppingListBtn: 'generateShoppingListBtn',
+    savedListsContainer: 'savedListsContainer',
+    savedListsList: 'savedListsList',
+    shoppingListDisplay: 'shoppingListDisplay',
+    shoppingListResult: 'shoppingListResult',
+    saveCurrentListBtn: 'saveCurrentListBtn',
+    deleteCurrentListBtn: 'deleteCurrentListBtn',
+    backToSavedListsBtn: 'backToSavedListsBtn',
+    exportShoppingListTxtBtn: 'exportShoppingListTxtBtn',
+    importFileInput: 'importFileInput',
+    importBtn: 'importBtn',
+    exportBtn: 'exportBtn',
+    suggestBtn: 'suggestBtn',
+    favoritesBtn: 'favoritesBtn',
+    recipesBtn: 'recipesBtn',
+    shoppingListBtn: 'shoppingListBtn',
+    addDishBtn: 'addDishBtn',
+    prevMonth: 'prevMonth',
+    nextMonth: 'nextMonth',
+    todayBtn: 'todayBtn',
+    calendarWrap: 'calendarWrap',
+    viewToggleButtons: '#viewToggle button',
+    exportOptions: '.export-option'
+  }
+};
