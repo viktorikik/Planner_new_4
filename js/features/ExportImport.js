d66a73fd58bb96d70556c04018a16ae12c590f55
viktorikik <z4dp89bzjn@privaterelay.appleid.com>
2026-09-15 22:25:40 +0700

    Create ExportImport.js
    Создание файла

---
+++ js/features/ExportImport.js
@@ -0,0 +1,10 @@
+import { DishStore } from '../stores/DishStore.js';
+import { RecipeStore } from '../stores/RecipeStore.js';
+import { CONSTANTS } from '../utils/Constants.js';
+import { Utils } from '../utils/Utils.js';
+import { showMessage } from '../utils/notifications.js';
+
+export function validateDish(dish, index) { /* ... */ }
+export function validateRecipe(recipe, index) { /* ... */ }
+export function exportData(format) { /* ... */ }
+export function importData(file) { /* ... */ }
