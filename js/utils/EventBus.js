ea06b184e8b4874019ce0c2a77552627ec7d63c8
viktorikik <z4dp89bzjn@privaterelay.appleid.com>
2026-09-18 00:05:23 +0700

    Update EventBus.js

--- js/utils/EventBus.js
+++ js/utils/EventBus.js
@@ -4,10 +4,6 @@ export const EventBus = {
     if (!this._events[event]) this._events[event] = [];
     this._events[event].push(callback);
   },
-  off(event, callback) {
-    if (!this._events[event]) return;
-    this._events[event] = this._events[event].filter(cb => cb !== callback);
-  },
   emit(event, data) {
     if (!this._events[event]) return;
     this._events[event].forEach(cb => cb(data));
